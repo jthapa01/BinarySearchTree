5c0602fb688ff15081f18bcd8ef7d37bfb9c9632
Jeevan <jthapa01@outlook.com>
2018-05-03 09:27:46 -0400

    Initial commit

---
+++ binarytree/Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree<string> tree =
+                new BinarySearchTree<string>();
+            tree.Insert("Telerik");
+            tree.Insert("Google");
+            tree.Insert("Microsoft");
+            tree.PrintTreeDFS();//Google Microsoft Telerik
+            Console.WriteLine(tree.Contains("Telerik"));//True
+            Console.WriteLine(tree.Contains("IBM"));//False
+            tree.Remove("Telerik");
+            Console.WriteLine(tree.Contains("Telerik"));//False
+            tree.PrintTreeDFS();//Google Microsoft
+            Console.ReadLine();
+
+        }
+    }
+}
